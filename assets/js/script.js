9d945b76f24ed6b512b20e1bef963f0e32054307
davyvanroy <davy.van.roy@gmail.com>
2017-11-16 13:27:06 +0100

    update counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,32 +29,32 @@ jQuery( document ).ready(function() {
         ts = (new Date()).getTime() + 10*24*60*60*1000;
     }
 
-    $('#countdown').countdown({
-        timestamp   : ts,
-        callback    : function(days, hours, minutes, seconds){
-
-            var message = "";
-
-            message += days + " day" + ( days==1 ? '':'s' ) + ", ";
-            message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
-            message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
-            message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
-
-        }
-    });
-    $('#countdownnav').countdown({
-        timestamp   : ts,
-        callback    : function(days, hours, minutes, seconds){
-
-            var message = "";
-
-            message += days + " day" + ( days==1 ? '':'s' ) + ", ";
-            message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
-            message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
-            message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
-
-        }
-    });
+    // $('#countdown').countdown({
+    //     timestamp   : ts,
+    //     callback    : function(days, hours, minutes, seconds){
+    //
+    //         var message = "";
+    //
+    //         message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+    //         message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+    //         message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+    //         message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+    //
+    //     }
+    // });
+    // $('#countdownnav').countdown({
+    //     timestamp   : ts,
+    //     callback    : function(days, hours, minutes, seconds){
+    //
+    //         var message = "";
+    //
+    //         message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+    //         message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+    //         message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+    //         message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+    //
+    //     }
+    // });
 
     // ========== VERTICAL SCROLL NAVIGATION ========== //
 
